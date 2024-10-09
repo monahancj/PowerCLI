a099d680692acc74e9dcb9505e4e370bb994c59a
Chris Monahan <monahancj@users.noreply.github.com>
2024-10-09 09:13:01 -0400

    Update Get-mVMAnnotationFormatted.ps1
    Formatting

--- mVMwarePowerCLI/InModule/vm/Get-mVMAnnotationFormatted.ps1
+++ mVMwarePowerCLI/InModule/vm/Get-mVMAnnotationFormatted.ps1
@@ -1,5 +1,5 @@
-﻿if (!(Get-Module -Name VMware.VimAutomation.Core)) { Import-Module VMware.VimAutomation.Core }
-<#
+﻿function Get-mVMAnnotationFormatted {
+	<#
     .SYNOPSIS
         A brief description of the function or script. This keyword can be used
         only once in each topic.
@@ -37,10 +37,6 @@
 		YYYMMDD username- 2nd comment.
 		YYYMMDD username- 3rd comment.
 
-ToDo
-----------------------
--Make move to decom folder work when connected to multiple vCenters.
-
 	.LINK
         The name of a related topic. The value appears on the line below
         the .LINK keyword and must be preceded by a comment symbol (#) or
@@ -54,47 +50,46 @@ ToDo
         (URI) to an online version of the same help topic. The online version 
         opens when you use the Online parameter of Get-Help. The URI must begin
         with "http" or "https".
-
 #>
 
-Function Get-mVMAnnotationFormatted {
-	
+	#TODO: Update comment based help.
+	#TODO: Update to match PowerShell function template.
+	#TODO: Update all output to the standard format using the snippet "OutputMessage".
+
 	[cmdletbinding(SupportsShouldProcess = $false)]
 	param (
-	[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]$VM
+		[Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]$VM
 	)
-	
+
 	begin {
-		
+
 		# code to be executed once BEFORE the pipeline is processed goes here
-		
+
 	} # end begin block
-	
+
 	process {
-		
+
 		Get-VM -Name $VM | Select-Object Name, `
 										 @{ n = 'Application'; e = { $_.CustomFields.Item("Application") } }, `
 										 @{ n = 'Business Unit'; e = { $_.CustomFields.Item("Business Unit") } }, `
 										 @{ n = 'Category'; e = { $_.CustomFields.Item("Category") } }, `
 										 @{ n = 'Support Contact'; e = { $_.CustomFields.Item("Support Contact") } }, `
+										 @{ n = 'Owner'; e = { $_.CustomFields.Item("Owner") } }, `
 										 @{ n = 'PCI'; e = { $_.CustomFields.Item("PCI") } }, `
 										 @{ n = 'PCI Role'; e = { $_.CustomFields.Item("PCI Role") } }
-		
-		
 	} #end of the process block
-	
+
 	end {
 		# code to be executed once AFTER the pipeline is processed goes here
-		
-		Remove-Variable VM
+
+		Remove-Variable VM -ErrorAction SilentlyContinue -WhatIf:$false # Using -WhatIf:$false to suppress unnecessary messages when a calling function has -Whatif:$true enabled.
 		[System.GC]::Collect() # Memory cleanup
-		
+
 	} #end of the end block
-	
-} # end function
 
 <# Comment History
 YYYMMDD username- 3rd comment.
 YYYMMDD username- 2nd comment.
 YYYMMDD username- 1st comment.
-#>
+#>	
+} # end function
